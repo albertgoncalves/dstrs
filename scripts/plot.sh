e9f65035476096c9ea9bc99933b1442e1116e7e2
Albert Goncalves <goncalves.albert@gmail.com>
2021-01-23 16:23:12 -0500

    rename scripts

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -4,5 +4,5 @@ set -eu
 
 "$WD/scripts/build.sh" "$1"
 "$WD/bin/$1" "$WD/out/$1.txt"
-"$WD/scripts/plot_hist.py" "$WD/out/$1.txt" "$WD/out/$1.png"
+"$WD/scripts/hist_kde.py" "$WD/out/$1.txt" "$WD/out/$1.png"
 feh "$WD/out/$1.png"
