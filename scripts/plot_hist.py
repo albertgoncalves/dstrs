992eca8128eaebeb74bc7db5cf12872f2d759ef1
albertgoncalves <goncalves.albert@gmail.com>
2021-01-23 10:48:27 -0500

    enhance plots

--- scripts/plot_hist.py
+++ scripts/plot_hist.py
@@ -3,33 +3,28 @@
 from sys import argv
 
 from matplotlib.pyplot import close, savefig, subplots, tight_layout
-from numpy import loadtxt
+from numpy import loadtxt, mean, median, std
 from seaborn import histplot
 
 
 def main():
     assert len(argv) == 3
     with open(argv[1], "r") as file:
-        if "." in file.readline():
-            dtype = "float32"
-            discrete = False
-            kde_kws = {
-                "bw_adjust": 1.75,
-            }
-        else:
-            dtype = "int32"
-            discrete = True
-            kde_kws = {
-                "bw_adjust": 5,
-            }
+        line = file.readline()
+    if "." in line:
+        xs = loadtxt(argv[1], dtype="float32", delimiter="\n")
+        kwargs = {"discrete": False, "kde_kws": {"bw_adjust": 1.75}}
+    else:
+        xs = loadtxt(argv[1], dtype="int32", delimiter="\n")
+        kwargs = {"discrete": True, "kde_kws": {"bw_adjust": 5}}
     (_, ax) = subplots()
-    histplot(
-        loadtxt(argv[1], dtype=dtype, delimiter="\n"),
-        discrete=discrete,
-        kde=True,
-        kde_kws=kde_kws,
-        edgecolor="w",
-        ax=ax,
+    histplot(xs, kde=True, edgecolor="w", ax=ax, **kwargs)
+    ax.set_title(
+        "  mean =>{:>9.4f}\n"
+        "median =>{:>9.4f}\n"
+        "   std =>{:>9.4f}".format(mean(xs), median(xs), std(xs)),
+        fontsize="large",
+        family="monospace",
     )
     tight_layout()
     savefig(argv[2])
