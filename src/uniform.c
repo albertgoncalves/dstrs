47344bb66f977f5013e0d45be4689dce03215ded
albertgoncalves <goncalves.albert@gmail.com>
2021-01-23 09:41:31 -0500

    rename file

--- src/uniform.c
+++ src/uniform.c
@@ -4,8 +4,6 @@
 
 const usize THRESHOLD = SIZE_BUFFER - 128;
 
-#define FMT "%.6f\n"
-
 i32 main(i32 n, const char** args) {
     EXIT_IF(n < 2);
     char* buffer = malloc(sizeof(char) * SIZE_BUFFER);
@@ -15,7 +13,8 @@ i32 main(i32 n, const char** args) {
         set_seed(&rng, get_microseconds(), 1);
         for (usize i = 0; i < 10000; ++i) {
             EXIT_IF(THRESHOLD < size);
-            i32 m = sprintf(&buffer[size], FMT, (f64)get_random_f32(&rng));
+            i32 m =
+                sprintf(&buffer[size], "%.6f\n", (f64)get_random_f32(&rng));
             EXIT_IF(m < 0);
             size += (usize)m;
         }
