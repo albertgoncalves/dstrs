aab53a4302a088120b1cdda2025c4e2f373d91d8
albertgoncalves <goncalves.albert@gmail.com>
2021-01-25 19:19:00 -0500

    add useful link

--- src/normal.h
+++ src/normal.h
@@ -12,6 +12,7 @@ static f32 get_random_normal(PcgRng* rng, f32 mu, f32 sigma) {
 }
 
 // NOTE: See `https://mathworld.wolfram.com/Box-MullerTransformation.html`.
+// NOTE: See `https://www.doc.ic.ac.uk/~wl/papers/07/csur07dt.pdf`.
 static PairF32 get_random_normal_pair(PcgRng* rng, f32 mu, f32 sigma) {
     EXIT_IF(sigma <= 0.0f);
     f32 a = sqrtf(-2.0f * logf(get_random_uniform_f32(rng)));
