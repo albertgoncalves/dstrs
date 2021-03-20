f679e78ed7de54876d2790934a102c5e8b2fc03a
albertgoncalves <goncalves.albert@gmail.com>
2021-03-20 11:46:25 -0400

    clean-up build script

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,13 +2,9 @@
 
 set -euo pipefail
 
-for x in "$WD"/src/*.h; do
-    cppcheck "$x" | sed 's/\/.*\/\(.*\) \.\.\./\1/g'
-done
 cppcheck \
     --enable=all \
     --suppress=missingIncludeSystem \
     "$WD/src" \
     | sed 's/\/.*\/\(.*\) \.\.\./\1/g'
-clang-format -i -verbose "$WD/src"/*.c 2>&1 | sed 's/\/.*\///g'
-clang-format -i -verbose "$WD/src"/*.h 2>&1 | sed 's/\/.*\///g'
+clang-format -i -verbose "$WD/src"/* 2>&1 | sed 's/\/.*\///g'
