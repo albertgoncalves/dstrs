8e9d7c81f6e996a1368b77dcf16eb8eb97963295
albertgoncalves <goncalves.albert@gmail.com>
2021-05-20 18:47:44 -0400

    demo f64 rng

---
+++ src/uniform_f64.c
@@ -0,0 +1,25 @@
+#include "pcg_rng.h"
+
+#define SIZE_BUFFER 1048576
+
+static const usize THRESHOLD_BUFFER = SIZE_BUFFER - 64;
+
+i32 main(i32 n, const char** args) {
+    EXIT_IF(n < 2);
+    char* buffer = malloc(sizeof(char) * SIZE_BUFFER);
+    usize size = 0;
+    {
+        PcgRng rng = get_rng();
+        set_seed(&rng, get_microseconds(), 1);
+        for (usize i = 0; i < 100000; ++i) {
+            EXIT_IF(THRESHOLD_BUFFER < size);
+            const i32 line =
+                sprintf(&buffer[size], "%.6f\n", get_random_uniform_f64(&rng));
+            EXIT_IF(line <= 0);
+            size += (usize)line;
+        }
+    }
+    set_file(args[1], buffer, size);
+    free(buffer);
+    return EXIT_SUCCESS;
+}
