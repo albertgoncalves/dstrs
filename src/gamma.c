44ae666ae905ed9fc897b9dc3798e0730957c2d1
Albert Goncalves <goncalves.albert@gmail.com>
2021-06-21 10:42:49 -0400

    descriptive var names

--- src/gamma.c
+++ src/gamma.c
@@ -13,14 +13,15 @@ i32 main(i32 n, const char** args) {
         set_seed(&rng, get_microseconds(), 1);
         // NOTE: See `https://en.wikipedia.org/wiki/Gamma_distribution`.
         const f32 theta = 2.0f;
+        const f32 alpha = 10.0f;
+        const f32 beta = 1.0f / theta;
         // NOTE: $ R
         //       > rgamma(10000, shape = `alpha`, scale = `beta`)
         for (usize i = 0; i < 10000; ++i) {
             EXIT_IF(THRESHOLD_BUFFER < size);
-            const i32 line =
-                sprintf(&buffer[size],
-                        "%.6f\n",
-                        (f64)get_random_gamma(&rng, 1.5f, 1.0f / theta));
+            const i32 line = sprintf(&buffer[size],
+                                     "%.6f\n",
+                                     (f64)get_random_gamma(&rng, alpha, beta));
             EXIT_IF(line <= 0);
             size += (usize)line;
         }
